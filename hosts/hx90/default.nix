8e7bfd081700709d9924b7862e1fbc8362615a83
Konstantin Labun <konstantin.labun@gmail.com>
2022-12-16 18:22:03 -0800

    enable kde

--- hosts/hx90/default.nix
+++ hosts/hx90/default.nix
@@ -12,6 +12,7 @@
 
     ./hardware-configuration.nix
     ./sway.nix
+    ./kde.nix
   ];
 
   # Bootloader.
@@ -66,7 +67,7 @@
   #programs.sway.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.enable = false; # use sdde + KDE
   services.xserver.desktopManager.gnome.enable = false;
 
   # Configure keymap in X11
---
+++ hosts/hx90/kde.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }: {
+  services.xserver.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  services.xserver.desktopManager.plasma5.excludePackages = with pkgs.libsForQt5; [
+    elisa
+    gwenview
+    okular
+    oxygen
+    khelpcenter
+    konsole
+    plasma-browser-integration
+    print-manager
+  ];
+}
